daddc27ce313e5c30a4e87b919dd6146d4390839
Mark Tomm <mark.tomm@martem.ee>
2018-05-13 22:48:10 +0300

    rewrite scripts [BASH]

--- lcbc/build/compile.sh
+++ lcbc/build/compile.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-rm -v ../bin/lcbcServer
-gcc --std=c99 ../../build/open62541.c ../lcbcServer.c ../gen/lcbc_feeder1.c -o ../bin/lcbcServer
+. config
+
+[[ -z ${GEN_FLAT_FEEDER} ]] || {
+    rm -v ${LCBC_ROOT}/bin/lcbcServerFlat
+    gcc --std=c99 ${OPEN_OPC_ROOT}/build/open62541.c ${LCBC_ROOT}/lcbcServer.c ${LCBC_ROOT}/gen/lcbc_feeder1.c -o ${LCBC_ROOT}/bin/lcbcServerFlat
+}
+
+[[ -z ${GEN_OBJECT_FEEDER} ]] || {
+    rm -v ${LCBC_ROOT}/bin/lcbcServerObject
+    gcc --std=c99 ${OPEN_OPC_ROOT}/build/open62541.c ${LCBC_ROOT}/lcbcServerObject.c ${LCBC_ROOT}/gen/lcbc_object_feeder1.c -o ${LCBC_ROOT}/bin/lcbcServerObject
+}
--- lcbc/build/xml_parser.sh
+++ lcbc/build/xml_parser.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
+. config
+
 ## initial minimal xml
-# rm -v ../gen/lcbc*
-# python ../../tools/nodeset_compiler/nodeset_compiler.py \
+# rm -v ${LCBC_ROOT}/gen/lcbc*
+# python ${LCBC_ROOT}/${LCBC_ROOT}/tools/nodeset_compiler/nodeset_compiler.py \
 # --types-array=UA_TYPES \
-# --existing ../../deps/ua-nodeset/Schema/Opc.Ua.NodeSet2.xml \
-# --xml ../xml/lcbc-feeder1.xml ../gen/lcbc_feeder1
+# --existing ${LCBC_ROOT}/${LCBC_ROOT}/deps/ua-nodeset/Schema/Opc.Ua.NodeSet2.xml \
+# --xml ${LCBC_ROOT}/xml/lcbc-feeder1.xml ${LCBC_ROOT}/gen/lcbc_feeder1
 
-# latest xml to code. xml is manualy composed. expect bugs
-rm -v ../gen/lcbc*
-python /usr/local/share/open62541/tools/nodeset_compiler/nodeset_compiler.py \
---types-array=UA_TYPES \
---existing /usr/local/include/open62541/deps/ua-nodeset/Schema/Opc.Ua.NodeSet2.xml \
---xml ../xml/lcbc-merge.xml ../gen/lcbc_feeder1
+[[ -z ${GEN_FLAT_FEEDER} ]] || {
+    # latest xml to code. xml is manualy composed. expect bugs
+    rm -v ${LCBC_ROOT}/gen/lcbc*
+    python /usr/local/share/open62541/tools/nodeset_compiler/nodeset_compiler.py \
+    --types-array=UA_TYPES \
+    --existing /usr/local/include/open62541/deps/ua-nodeset/Schema/Opc.Ua.NodeSet2.xml \
+    --xml ${LCBC_ROOT}/xml/lcbc-merge.xml ${LCBC_ROOT}/gen/lcbc_feeder1
 
-# aling xml parser output include with default insatll location
-sed -i 's/\(#include \).*$/\1 "open62541\/open62541.h"/' ../gen/lcbc_feeder1.h
+    # aling xml parser output include with default insatll location
+    sed -i 's/\(#include \).*$/\1 "open62541\/open62541.h"/' ${LCBC_ROOT}/gen/lcbc_feeder1.h
+}
+
+[[ -z ${GEN_OBJECT_FEEDER} ]] || {
+    # latest xml to code. xml is manualy composed. expect bugs
+    rm -v ${LCBC_ROOT}/gen/lcbc*
+    python /usr/local/share/open62541/tools/nodeset_compiler/nodeset_compiler.py \
+    --types-array=UA_TYPES \
+    --existing /usr/local/include/open62541/deps/ua-nodeset/Schema/Opc.Ua.NodeSet2.xml \
+    --xml ${LCBC_ROOT}/xml/lcbc-feeder-object.xml ${LCBC_ROOT}/gen/lcbc_object_feeder1
+
+    # aling xml parser output include with default insatll location
+    sed -i 's/\(#include \).*$/\1 "open62541\/open62541.h"/' ${LCBC_ROOT}/gen/lcbc_object_feeder1.h
+}
+
